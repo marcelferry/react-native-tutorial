7f019cf06458c2173a38da0f6bb95e979f7b2f40
Marcelo Ferreira da Silva <marcelferry@gmail.com>
2017-06-13 18:15:52 -0300

    First Commit

---
+++ SearchResults.js
@@ -0,0 +1,94 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableHighlight,
+  ListView,
+  Text
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
+
+
+class SearchResults extends Component {
+  static navigationOptions = {
+    title: 'Results',
+  };
+
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1.lister_url !== r2.lister_url});
+    this.state = {
+      dataSource: dataSource.cloneWithRows(this.props.navigation.state.params.listings)
+    };
+  }
+
+  renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
+    return (
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+          underlayColor='#dddddd'>
+        <View>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View  style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  rowPressed(listerURL) {
+    var property = this.props.navigation.state.params.listings.filter(prop => prop.lister_url === listerURL)[0];
+    this.props.navigation.navigate('PropertyView', {property: property} );
+  }
+
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}/>
+    );
+  }
+
+}
+
+module.exports = SearchResults;
---
+++ index.android.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var ReactNavigation = require('react-navigation');
+
+var SearchPage = require('./SearchPage');
+
+var SearchResults = require('./SearchResults');
+
+var PropertyView = require('./PropertyView');
+
+var MapsPage = require('./MapsPage');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class HelloWorld extends React.Component {
+  render() {
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+  }
+
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <ReactNative.View>
+        <ReactNative.Text style={styles.text}>Hello, Navigation!</ReactNative.Text>
+        <ReactNative.Button onPress={() => navigate('SearchPage')} title="Search Page"/>
+      </ReactNative.View>
+    );
+  }
+}
+
+const PropertyFinderApp = ReactNavigation.StackNavigator({
+  Home: { screen: HomeScreen },
+  SearchPage: { screen: SearchPage },
+  SearchResults: { screen: SearchResults },
+  PropertyView: { screen: PropertyView },
+  MapsPage: { screen: MapsPage }
+});
+
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', () => PropertyFinderApp );
